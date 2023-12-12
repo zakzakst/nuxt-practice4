97e82a9e4fc42a5cbfae098b5dbb69d2ec2ebeb3
zakzakst <e1433104@gmail.com>
2023-12-12 23:50:43 +0900

    修正

---
+++ components/organisms/items.vue
@@ -0,0 +1,38 @@
+<template>
+  <section>
+    <article v-for="item in items">
+      <NuxtLink :to="`/items/${item.id}`" @click.native="active = item.id">
+        <img
+          :src="`https://picsum.photos/id/${item.src}/200/300`"
+          alt=""
+          :class="{ active: active === item.id }"
+        />
+      </NuxtLink>
+    </article>
+  </section>
+</template>
+
+<script setup>
+const active = useState();
+const items = ref([
+  {
+    id: 237,
+  },
+  {
+    id: 238,
+  },
+  {
+    id: 239,
+  },
+  {
+    id: 240,
+  },
+]);
+</script>
+
+<style scoped>
+img.active {
+  view-transition-name: selected-item;
+  contain: layout;
+}
+</style>
---
+++ pages/items/[id].vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <img :src="`https://picsum.photos/id/${route.params.id}/200/300`" alt="" />
+  </section>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<style scoped>
+h1 {
+  view-transition-name: header;
+}
+img {
+  view-transition-name: selected-item;
+}
+</style>
+
+<style>
+::view-transition-old(header),
+::view-transition-new(header) {
+  width: auto;
+}
+</style>
